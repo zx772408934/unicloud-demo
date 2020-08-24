5f1c84f6e62bb545f4d571011c561989c8e803f5
unknown <772408934@qq.com>
2020-08-24 23:18:58 +0800

    初始化项目

---
+++ cloudfunctions-aliyun/login/index.js
@@ -0,0 +1,22 @@
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	
+	if(!(/^[1][3,4,5,6,7,8,9][0-9]{9}$/).test(event.phone)){
+	    return {
+			code:201,
+			msg:'手机号格式错误'
+		}
+	}
+	if(!(/^[0-9]{6}$/).test(event.code)){
+	    return {
+			code:202,
+			msg:'密码格式错误'
+		}
+	}
+	
+	
+	//返回数据给客户端
+	// return event
+};
